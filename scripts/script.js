45ceb3eb6f010984967d078aa2e68c12920c2999
a-chavez474 <abelinkpact@gmail.com>
2022-09-29 01:09:04 -0600

    Updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-// Get & Store Date
-var today = new Date();
-today = today.getFullYear();
 
-// Check Date
-console.log(today);
-
-// Display Date
-$('.year').text(today);
+window.onload = function() {
+    let now = new Date();
+    let month = now.getMonth() + 1;
+    let day = now.getDate();
+    let year = now.getFullYear();
+    let dateTime = month + "/" + day + "/" + year + " " + now.toLocaleTimeString();
+    document.getElementById("time").textContent = dateTime;
+  }
