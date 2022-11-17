467368c4e86ca82e55327f16c01cecf722a83f9f
a-chavez474 <abelinkpact@gmail.com>
2022-11-16 17:09:51 -0700

    changes

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,20 +1,9 @@
-window.onload = function() {
-  let now = new Date();
-  let month = now.getMonth() + 1;
-  let day = now.getDate();
-  let year = now.getFullYear();
-  let dateTime = month + "/" + day + "/" + year + " " + now.toLocaleTimeString();
-  document.getElementById("time").textContent = dateTime;
+
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
 }
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-})
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("clik", () =>{
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  }
-))
+const x = document.getElementById("hamburgerBtn")
+x.onclick = toggleMenu;
+
